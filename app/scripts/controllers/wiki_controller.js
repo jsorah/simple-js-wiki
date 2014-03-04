91ec9a3b83aae2f8d064446d66c9958564c56b89
Joshua Sorah <jsorah@gmail.com>
2014-03-03 20:30:07 -0500

    Adding more robust editing capability.

--- app/scripts/controllers/wiki_controller.js
+++ app/scripts/controllers/wiki_controller.js
@@ -2,11 +2,22 @@ Website.WikiController = Em.ObjectController.extend({
     editing: false,
     actions: {
         saveChanges: function() {
-            alert("Saved!");
             this.set('editing',false);
+            
+            var model = this.get('model');
+            
+            /* Update modified date */
+            model.set('modified', new Date());
+            model.save();
+
+            
         },
         edit: function() {
             this.set('editing',true);
+        },
+        cancel: function() {
+            this.set('editing',false);
+            this.get('model').rollback();
         }
     }
 });
